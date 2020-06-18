0dc0bc785a9750ab2a27218e6a3e6d8569f6649f
Marcelo Lemos <marcelo@vpeventos.com>
2020-06-18 11:33:19 -0300

    primeiro commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:lojaonline/tabs/home_tab.dart';
+import 'package:lojaonline/tabs/products_tab.dart';
+import 'package:lojaonline/widgets/custom_drawer.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 117, 40, 212),
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:lojaonline/tiles/drawer_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  final PageController pageController;
+
+  CustomDrawer(this.pageController);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildDrawerBack() => Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 117, 40, 212),
+            Color.fromARGB(255, 23, 9, 41)
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        );
+
+    return Drawer(
+      child: Stack(
+        children: <Widget>[
+          _buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 32.0, top: 16.0),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 8.0),
+                padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
+                height: 170.0,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: 8.0,
+                      left: 0.0,
+                      child: Text(
+                        "Loja Online",
+                        style: TextStyle(
+                            fontSize: 34.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Olá,",
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              "Entre ou cadastre-se",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(),
+              DrawerTile(Icons.home, "Início", pageController, 0),
+              DrawerTile(Icons.list, "Produtos", pageController, 1),
+              DrawerTile(Icons.location_on, "Lojas", pageController, 2),
+              DrawerTile(Icons.playlist_add_check, "Meus pedidos", pageController, 3),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
